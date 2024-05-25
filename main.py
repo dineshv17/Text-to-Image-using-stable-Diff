80d5d79173f374e99808ba5bd1828edd9e6076b6
dineshv17 <98011287+dineshv17@users.noreply.github.com>
2024-05-25 17:57:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import torch
 from torch import autocast
 from diffusers import StableDiffusionPipeline
 
+auth_token = "Your_Hugging_face_api_Key"
 # create a app
 app = tk.Tk()
 app.title("Text to Image using Stable Diffusion")
@@ -16,12 +17,6 @@ prompt.place(x=10, y=10)
 
 lmain = ctk.CTkLabel(master=app,width=512, height=512)
 lmain.place(x=10, y=110) 
-
-button = ctk.CTkButton(master=app,width=120, height=40, font=("Arial", 12),text_color="black", fg_color="white")           
-button.place(x=206, y=60)
-button.configure(text = "Generate")
-app.mainloop()
-auth_token = "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"
 
 modelid = "CompVis/stable-diffusion-v1-4"
 pipe = StableDiffusionPipeline.from_pretrained(modelid, version = "fp16",torch_dtype=torch.float16,use_auth_token=auth_token)
@@ -34,3 +29,8 @@ def generate():
     image = ImageTk.PhotoImage(img)
     image.save("output.png")
     lmain.configure(image=image)
+
+button = ctk.CTkButton(master=app,width=120, height=40, font=("Arial", 12),text_color="black", fg_color="white")           
+button.place(x=206, y=60)
+button.configure(text = "Generate")
+app.mainloop()
